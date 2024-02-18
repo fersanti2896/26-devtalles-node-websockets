5de14c931d5361000aa8f36451efdcfd2f920905
Fernando Nicolás <fersanti2896@gmail.com>
2024-02-18 02:36:24 -0600

    Aplicación de WS inicial.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,22 @@
 
-console.log('Hola mundo');
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 5796 });
+
+wss.on('connection', function connection(ws) {
+    console.log('Client connected')
+
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('Hola desde el servidor.');
+
+    setInterval(() => {
+        ws.send('Hola de nuevo')
+    }, 2000);
+});
+
+console.log('http://localhost:5796')
