00119984bd798044d4adc7f5c5b9029d4a39b7e4
Fernando Nicolás <fersanti2896@gmail.com>
2024-02-18 20:00:19 -0600

    Enviando mensajes al servidor y broadcast.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 5796 });
 
@@ -8,11 +8,24 @@ wss.on('connection', function connection(ws) {
 
     ws.on('error', console.error);
 
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
+    ws.on('message', function message( data ) {
+        console.log('Desde el cliente', data);
+
+        const payload = JSON.stringify({
+            type: 'custom-message',
+            payload: data.toString()
+        });
+
+        // ws.send( JSON.stringify( payload ) );
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(payload, { binary: false });
+            }
+        });
     });
 
-    ws.send('Hola desde el servidor.');
+    // ws.send('Hola desde el servidor.');
 
     ws.on('close', () => {
         console.log('Client disconnected')
